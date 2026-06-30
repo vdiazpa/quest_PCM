4978530b005a6c9131afabd2be9cc1246d5b689b
vdiazpa <vdiazpa@sandia.gov>
2026-06-30 09:21:14 -0600

    added mono solve

--- example_script.py
+++ example_script.py
@@ -1,14 +1,15 @@
-import logging
-import os
+
+from pyomo.environ import Objective, value
 from egret.common.log import logger as egret_logger
 from pcm.data_manager.data_main import DataManager
 from pcm.market_manager.market_main import MarketSimulator
-from pcm.result_manager.result_main import ResultManager# %%
-from egret.data.model_data import ModelData
-from RH_utils import *
-import csv 
+# from RH_utils import *
+import logging
+import time
+
 
 egret_logger.setLevel(logging.ERROR)
+
 main_data_path = "Data/RTS_GMLC"
 yaml_path = "config/GMLC_config.yaml"
 
@@ -21,10 +22,37 @@ simulator.create_DA_RT_models()
 md_full = simulator.DA_model.clone()
 md_full.data["current_market"] = "DA"
 
-rh_mod, _, fixed_sol, times = run_RH_egret(
-    md_full, F=12, L=12, 
-    model_generator = simulator.egret_uc_model_generator, 
-    uc_solver = simulator.uc_solver)
+#============================================= monolithic DA solve
+
+t0 = time.perf_counter()
+da_mod = simulator.egret_uc_model_generator(md_full)
+t1 = time.perf_counter() - t0    #build time
+
+pyomo_sol, _, _ = simulator.egret_uc_solver(
+    da_mod, 
+    solver="gurobi",
+    mipgap=input_manager.config.get("mipgap", 0.01), 
+    timelimit=None, 
+    solver_tee=False, 
+    symbolic_solver_labels=False, 
+    solver_options=None, 
+    solve_method_options=None, 
+    relaxed=False)
+
+t2 = time.perf_counter() - t1
+
+mono_obj = value(next(da_mod.component_data_objects(Objective, active=True)))
+
+#=============== Print mono results
+
+print("MONO OBJECTIVE:", mono_obj)
+print("MONO BUILD TIME (secs):", t1)
+print("MONO SOLVE TIME (secs):", t2)
+
+# rh_mod, _, fixed_sol, times = run_RH_egret(
+#     md_full, F=12, L=12, 
+#     model_generator = simulator.egret_uc_model_generator, 
+#     uc_solver = simulator.uc_solver)
 
 
 # da_mod = simulator.egret_uc_model_generator(md)
