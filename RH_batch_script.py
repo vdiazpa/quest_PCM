259c396a2c18572b232f21106065587cd05fbe65
vdiazpa <vdiazpa@sandia.gov>
2026-09-09 13:56:48 -0600

    preparing code for benchmarking

--- RH_batch_script.py
+++ RH_batch_script.py
@@ -31,8 +31,8 @@ opt_gaps   = [0.01, 0.001]
 md_full = simulator.DA_model.clone()
 md_full.data["current_market"] = "DA"
 
-#==================================Build and solve mono to 0% optimality gap. Time it.
-
+#==================================Build and solve non-lazy mono & solve to 0% optimality gap. Time it.
+t0 = time.perf_counter()
 da_mod_mono = simulator.egret_uc_model_generator(md_full, ptdf_options={"lazy": False})   # pyomo model with quest storage constraints0
 
 t_mono_st     = time.perf_counter()
@@ -47,12 +47,15 @@ rows = []
 for g in opt_gaps: 
 
     #============================================Build & solve DA w. lazy PTDF algorithm. Time it.
-    t_lazy_st   = time.perf_counter() 
-    lazy_mod    = simulator.egret_uc_model_generator(md_full, ptdf_options={"lazy": True})   
+    t_lazy_st0   = time.perf_counter() 
+    lazy_mod    = simulator.egret_uc_model_generator(md_full, ptdf_options={"lazy": True})  
+    t_lazy_st   = time.perf_counter()  
+
     p_sol, _, _ = simulator.egret_uc_solver(lazy_mod, solver="gurobi", mipgap=g, timelimit=None, solver_tee=False, symbolic_solver_labels=False, solver_options=None, solve_method_options=None, relaxed=False)
     lazy_obj    = value(next(lazy_mod.component_data_objects(Objective, active=True)))
-    t_lazy_end  = time.perf_counter()     
-    t_lazy      = t_lazy_end - t_lazy_st
+    t_lazy_end  = time.perf_counter()   
+
+    t_lazy  = t_lazy_end - t_lazy_st0
 
     for t in FandLs: 
         for r in relax_look:
@@ -62,7 +65,24 @@ for g in opt_gaps:
             t_rh_end   = time.perf_counter()
             rh_time    = t_rh_end - t_rh_start 
             print(f"F={F}, L={L}, relax_lookahead={r}, RH_opt_gap={g}, RH windows solve (secs): {round(rh_time,4)}")
-            rows.append({"F": F, "L": L, "relax_lookahead": r, "opt_gap": g, "RH_solve_time": rh_time, "RH_objective": res["rh_objective"], "lazy_solve_time": t_lazy, "lazy_objective": lazy_obj, "mono_solve_time": t_mono_end, "mono_objective": mono_obj, "rh_build_time": res["rh_build_time"], "rh_solve_time": res["rh_solve_time"], "rh_dispatch_build": res["t_dispatch_build"], "rh_dispatch_solve": res["t_dispatch_solve"]})
+            rows.append({
+                "F": F, 
+                "L": L, 
+                "relax_lookahead": r, 
+                "opt_gap": g, 
+                "RH_total_time": rh_time, 
+                "RH_objective": res["rh_objective"], 
+                "lazy_solve_time": t_lazy, 
+                "lazy_build_time": t_lazy_st-t_lazy_st0,
+                "lazy_time": t_lazy,
+                "lazy_objective": lazy_obj, 
+                "mono_build_time": t0-t_mono_st,
+                "mono_solve_time": t_mono_st-t_mono_end,
+                "mono_objective": mono_obj, 
+                "rh_build_time": res["rh_build_time"], 
+                "rh_solve_time": res["rh_solve_time"], 
+                "rh_dispatch_build": res["t_dispatch_build"], 
+                "rh_dispatch_solve": res["t_dispatch_solve"]})
 
 
 
